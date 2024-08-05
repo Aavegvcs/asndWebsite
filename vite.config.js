826af4d7a63e507404ea0704a71ee6bc049e892b
Gaurav <gaurav.gangola@aaveg.com>
2024-08-05 14:36:43 +0530

    comment ip server script

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/",
-  // plugins: [react()],
-  // preview: {
-  //   port: 80,
-  //   strictPort: true,
-  // },
-  // server: {
-  //   port: 80,
-  //   strictPort: true,
-  //   host: true,
-  //   origin: "http://18.60.9.40:80",
-  // },
+  base: "/",
+  plugins: [react()],
+  preview: {
+    port: 80,
+    strictPort: true,
+  },
+//   server: {
+//     port: 80,
+//     strictPort: true,
+//     host: true,
+//     origin: "http://18.60.9.40:80",
+//   },
 })
