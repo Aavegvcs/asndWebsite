96f033d5b647a2db83d9d6c817db409016f1fe40
Gaurav <gaurav.gangola@aaveg.com>
2024-08-05 14:33:53 +0530

    fix issse

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Home from "./pages/Home/Home";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react'
+import Home from './pages/Home/Home'
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Home />
-      </BrowserRouter>
-    </>
-  );
+    <div>
+      <Home />
+    </div>
+  )
 }
 
-export default App;
+export default App
